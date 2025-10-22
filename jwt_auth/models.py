64ea0106e1ff6192007768fd49d5aae0d11c90fb
shaexintran <shaexin@icloud.com>
2025-10-22 10:37:47 -0700

    Added defaults to images

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -7,4 +7,4 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50)
     # Changing to default address to null,blank since we don't ask for it during sign up
     address = models.CharField(max_length=150, null=True, blank=True)
-    profile_image = models.CharField(max_length=300, null=True, blank=True)
+    profile_image = models.CharField(max_length=300, null=True, blank=True, default="")
