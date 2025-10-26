94c2e3941fc5ae658f4bee1441c2660e8f42f7a3
Bronts <bronteda@gmail.com>
2025-10-26 18:36:03 +0000

    removed serializer

--- products/serializers/populated.py
+++ products/serializers/populated.py
@@ -3,11 +3,6 @@ from jwt_auth.serializers import UserSerializer
 from shops.serializers.populated import PopulatedShopSerializer
 
 
-class PopulatedProductImageSerializer(ProductImageSerializer):
-    # Keep flat image representation when nested under a product
-    pass
-
-
 class PopulatedProductSerializer(ProductSerializer):
     owner = UserSerializer()
     shop = PopulatedShopSerializer()
